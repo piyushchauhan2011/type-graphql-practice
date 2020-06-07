dacea9703e2c043c2112354430b53a20361e5212
Piyush Chauhan <piyushchauhan2011@gmail.com>
2020-06-07 22:54:07 +0700

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { RegisterResolver } from "./modules/user/Register";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { redis } from "./redis";
+import cors from "cors";
+import { LoginResolver } from "./modules/user/Login";
+import { MeResolver } from "./modules/user/Me";
+
+const main = async () => {
+  await createConnection();
+
+  const schema = await buildSchema({
+    resolvers: [MeResolver, RegisterResolver, LoginResolver],
+  });
+
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req }) => ({ req }),
+  });
+
+  const app = express();
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:4000",
+    })
+  );
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis,
+      }),
+      name: "qid",
+      secret: "keyboard cat",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+      },
+    })
+  );
+
+  // app.use(
+  //   session({
+  //     secret: "keyboard cat",
+  //     resave: false,
+  //     saveUninitialized: true,
+  //     cookie: { secure: false },
+  //   })
+  // );
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on http://localhost:4000/graphql");
+  });
+};
+
+main();
---
+++ src/modules/user/register/isEmailAlreadyExist.ts
@@ -0,0 +1,30 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraintInterface,
+  ValidatorConstraint,
+} from "class-validator";
+import { User } from "../../../entity/User";
+
+@ValidatorConstraint({ async: true })
+export class IsEmailAlreadyExistConstraint
+  implements ValidatorConstraintInterface {
+  validate(email: string) {
+    return User.findOne({ where: { email } }).then((user) => {
+      if (user) return false;
+      return true;
+    });
+  }
+}
+
+export function isEmailAlreadyExist(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsEmailAlreadyExistConstraint,
+    });
+  };
+}
